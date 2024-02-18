911434ca1c34edbc02f1575ce8b2631bab6fd7dd
unknown <girisurendra321@gmail.com>
2024-02-18 09:46:53 +0545

    Prcatice

---
+++ basicpractice.kt
@@ -0,0 +1,5 @@
+class basicpractice {
+    fun main(){
+        print("Hello, My name is surendra giri. I am a kotlin developer.")
+    }
+}
---
+++ basics.kt
@@ -0,0 +1,4 @@
+fun  main(agrs: Array<String>){
+    
+    print("HEllO, Everyone Its me Surendra Giri. I am a Kotlin Developer")
+}
